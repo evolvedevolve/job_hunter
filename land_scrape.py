450afe688aeaf5e72b6a319f62613be736418d5f
Pattywhack Crosman <patrick.crosman@gmail.com>
2021-12-03 14:38:23 -0700

    outputs info now
    this creates the csv woohoo

--- land_scrape.py
+++ land_scrape.py
@@ -11,7 +11,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 import re
-import requests
+import pandas as pd
 
 url = "https://suite.insala.com/ICM/Calendar/EventadminV5.aspx"
 
@@ -29,7 +29,8 @@ user_name_field.send_keys("patrick.crosman@gmail.com")
 
 # <input type="password" id="txtPassword" name="Password" maxlength="50">
 user_password_field = browser.find_element(By.ID, "txtPassword")
-user_password_field.send_keys("")
+user_password_field.send_keys(open(
+    r"H:\2021-11-03-HIGHER-LANDING\sensitive.txt", "r").read())
 
 # <input name="btnLogin" type="submit" id="btnLogin" class="green" value="Login">
 login_button = browser.find_element(By.ID, "btnLogin")
@@ -53,25 +54,44 @@ event_title_links = page_soup.find_all("a", {"id" : re.compile('rptUpcomingEvent
 # slap all required sessions into one string for easy compare
 required_sessions_file = open(r"H:\2021-11-03-HIGHER-LANDING\schedule.txt", "r")
 required_sessions_string = required_sessions_file.read()
- 
+
+events_to_attend_list = []
+
+
 # check which ones are the right dates for my enrollment
 for link in event_title_links:
     if(link.text in required_sessions_string):
+        '''link.text, event_time, zoom_link, description  '''
         #print(link['id'])
+        print(link.text)
         click_me = browser.find_element(By.ID, link['id'])
         click_me.click()
         rsvp_page_ready = waiter.until(EC.element_to_be_clickable((By.ID, 'btnRSVP')))
         
-        tmp_event_soup = BeautifulSoup(browser.page_source, 'html.parser')
-        # give all text in div class events-calendar 
-        # or all text in div class column wpc65 left
-        tmp_event_column = tmp_event_soup.find(class_='column wpc65 left')
-        p_tags = tmp_event_column.find_all(["p","span"])
+        tmp_event_soup = BeautifulSoup(browser.page_source, 'html.parser')        
+        zoom_link = tmp_event_soup.find('p', text=re.compile('^https:.*zoom'))        
+        # get the parent/containing element
+        description_container_element = zoom_link.parent        
         
-        print(p_tags) 
-        # , <span id="lblDescription"></span>,  everything after that
+        event_start_datetime = description_container_element.find(id='lblDatetime')
+        #print(event_start_datetime.text.replace('/','-'))
+        event_start_datetime_formatted = event_start_datetime.text.replace('/','-')
+                
+        # find the start point that we are interested in
+        text_start_point = description_container_element.find(id='lblDescription')        
+        text_end_point = description_container_element.find(text=re.compile(".*Passcode:"))        
         
+        actual_description_text = description_container_element.get_text(
+            ).split("Send calendar to email")[1].strip('\n')
+                
+        #print(actual_description_text)
+        #zoom_link_text = zoom_link.text
+        #print(zoom_link_text)        
         
+        tmp_tuple = (link.text, event_start_datetime_formatted,
+                     zoom_link.text, actual_description_text)
+        events_to_attend_list.append(tmp_tuple)
+    
         back_to_calendar_link = browser.find_element(By.ID, "hlkBackToCalendar")
         back_to_calendar_link.click()
         tmp_calendar_ready = waiter.until(EC.element_to_be_clickable((By.ID, 'lnkUpcomingEvents')))
@@ -79,7 +99,11 @@ for link in event_title_links:
         tmp_upcoming_events_link.click()
         tmp_upcoming_events_page_ready = waiter.until(EC.element_to_be_clickable((By.ID, 'rptUpcomingEvents_ctl00_btnRSVP')))
     else:
-        print("skipped")
+        print("skipped {}".format(link.text))
+
+events_to_attend_df = pd.DataFrame(events_to_attend_list, columns=["Title", "Start Time", "Link", "Description"])
+print(events_to_attend_df.head())
+events_to_attend_df.to_csv('Required_Events.csv')
 
 '''
 these are in class="row pdb20"
@@ -89,8 +113,56 @@ these are in class="row pdb20"
 
 give me all p that are siblings of class="column wpc65 left"
 
-'''
 
+       # give all text in div class events-calendar 
+        # or all text in div class column wpc65 left
+        # tmp_event_column = tmp_event_soup.find(class_='column wpc65 left')
+        # can find this by getting the parent of the zoom link
+        
+        # the above line probably doesnt work
+        
+        #p_tags = tmp_event_column.find_all("p") # [,"span"]
+        #p_spans = tmp_event_column.find_all(["p","span"])
+        #spans = tmp_event_column.find_all('span')
+        # start of the description
+        # <span id="lblDescription"></span>, 
+        #print(description_container_element.get_text())
+        
+        #text_i_want = (meeting_start_datetime, description_text, zoom_link)
+        
+        #for p in range(text_start_point.sourceline, text_end_point.sourceline):
+        #print(text_end_point.next_element().text)
+        
+        #text_i_want = explicit_for(list_of_things, conditions("lblDescription", "web.zoom.us"))
+        #text_i_want = 
+        # siblings to keep is the index start and end 
+        
+        
+        for text_element in text_start_point.next_elements:
+            tmp_text = repr(text_element)
+            
+            if len(tmp_text) > 0:
+                print(repr(tmp_text))
+                if repr(text_end_point) == text_element:
+                    print('reached end of description')
+            
+            # get our tag text
+            #tmp_text = tag.get_text() # if text exits is not empty or contains
+            #if len(tmp_text) > 0:
+            #print(tmp_text)
+            # add this to a the file for the page
+        
+        
+        #print(p_tags)
+        
+        # , <span id="lblDescription"></span>,  everything after that
+        # get next sibling until Passcode: or zoom
+        
+        # this is the zoom meeting link web.zoom.us
+        #>Meeting ID: 816 4508 9906</p>,   can stop after this 
+        #</span></p>, <span>Passcode: 
+        
+'''
 #for link in enumerate(filtered_links)
 #tmp_url = filtered_links[0].href
 #result = requests.get(tmp_url)
